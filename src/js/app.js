0f220286327758981c37f9aae7a4990b590860fb
Kaeserkiev <kaeserkiev@gmail.com>
2016-02-20 15:40:23 +0200

    add about and contact pages

--- src/js/app.js
+++ src/js/app.js
@@ -31,4 +31,20 @@ $(function () {
 		}
 	});
 
+
+	//setup smooth scrolling
+	var $jsLink = $('.js-link')
+	$jsLink.click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
 });
