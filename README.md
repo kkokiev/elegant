4eadaedd00a522ec472863582bf45a6c3bd925a1
Kaeserkiev <kaeserkiev@gmail.com>
2016-02-23 07:33:26 +0200

    topline fix

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<h1>Few portfolio template pages coded with stylus, jade, jquery, BEM </h1>
+<h2>Few portfolio template pages coded with stylus, jade, jquery, BEM </h2>
--- src/js/app.js
+++ src/js/app.js
@@ -48,11 +48,11 @@ $(function () {
 	var $jsLink = $('.js-link')
 	$jsLink.click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
+			var $target = $(this.hash);
+			$target = $target.length ? $target : $('[name=' + this.hash.slice(1) +']');
+			if ($target.length) {
 				$('html,body').animate({
-					scrollTop: target.offset().top
+					scrollTop: $target.offset().top
 				}, 1000);
 				return false;
 			}
